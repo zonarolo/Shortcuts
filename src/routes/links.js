a9a77b91546181412664edd9777c9b9048fdef0a
Juan Rolo <juanluis211@outlook.com>
2021-01-30 21:51:44 +0100

    Post a link

--- src/routes/links.js
+++ src/routes/links.js
@@ -7,8 +7,16 @@ router.get("/add", (req, res) => {
   res.render("links/add");
 });
 
-router.post('/add', (req, res) => {
-  res.send('Recibido');
+router.post("/add", async (req, res) => {
+  const { title, url, description } = req.body;
+  const newLink = {
+    title,
+    url,
+    description,
+  };
+
+  await pool.query("INSERT INTO links set ?", [newLink]);
+  res.send("Recibido");
 });
 
 module.exports = router;
