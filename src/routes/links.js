43061940b774520dc10335c69fa70cf94cc01045
Juan Rolo <juanluis211@outlook.com>
2021-01-30 22:57:47 +0100

    Updating Data from MySQL

--- src/routes/links.js
+++ src/routes/links.js
@@ -31,4 +31,23 @@ router.get("/delete/:id", async (req, res) => {
   res.redirect("/links");
 });
 
+router.get("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const links = await pool.query("SELECT * FROM links WHERE id = ?", [id]);
+  res.render("links/edit", { link: links[0] });
+});
+
+router.post("/edit/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, description, url } = req.body;
+  const newLink = {
+    title,
+    description,
+    url,
+  };
+  console.log(newLink);
+  await pool.query("UPDATE links set ? WHERE id = ?", [newLink, id]);
+  res.redirect("/links");
+});
+
 module.exports = router;
