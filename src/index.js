9c3ddc83e5c089588c3c77c9a5db8a90a9f08d41
Juan Rolo <juanluis211@outlook.com>
2021-01-31 13:23:55 +0100

    Add SignUp Page and Hash into MySQL

--- src/index.js
+++ src/index.js
@@ -1,42 +1,48 @@
-const express = require("express");
-const morgan = require("morgan");
-const exphbs = require("express-handlebars");
-const path = require("path");
-const flash = require("connect-flash");
-const session = require("express-session");
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const flash = require('connect-flash');
+const session = require('express-session');
 const MySQLStore = require('express-mysql-session');
+const passport = require('passport');
 
 const { database } = require('./keys');
 
 // Initializations
 const app = express();
+require('./lib/passport');
 
 // Settings
-app.set("port", process.env.PORT || 4000);
-app.set("views", path.join(__dirname, "views"));
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
 app.engine(
-  ".hbs",
+  '.hbs',
   exphbs({
-    defaultLayout: "main",
-    layoutsDir: path.join(app.get("views"), "layouts"),
-    partialsDir: path.join(app.get("views"), "partials"),
-    extname: ".hbs",
-    helpers: require("./lib/handlebars"),
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars'),
   })
 );
-app.set("view engine", ".hbs");
+app.set('view engine', '.hbs');
 
 // Middlewares
-app.use(session({
-  secret: 'juanmysqlnodesession',
-  resave: false,
-  saveUninitialized: false,
-  store: new MySQLStore(database)
-}));
+app.use(
+  session({
+    secret: 'juanmysqlnodesession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database),
+  })
+);
 app.use(flash());
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Global Variables
 app.use((req, res, next) => {
@@ -45,14 +51,14 @@ app.use((req, res, next) => {
 });
 
 // Routes
-app.use(require("./routes"));
-app.use(require("./routes/authentication"));
-app.use("/links", require("./routes/links"));
+app.use(require('./routes'));
+app.use(require('./routes/authentication'));
+app.use('/links', require('./routes/links'));
 
 // Public
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Starting the server
-app.listen(app.get("port"), () => {
-  console.log("Server on port ", app.get("port"));
+app.listen(app.get('port'), () => {
+  console.log('Server on port ', app.get('port'));
 });
