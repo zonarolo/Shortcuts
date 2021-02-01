c94bf7b2b82301d2c256c5587f1081efffb48679
Juan Rolo <juanluis211@outlook.com>
2021-02-01 01:47:58 +0100

    Protecting Routes from the Server

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const passport = require('passport');
+const { isLoggedIn, isNotLoggedIn } = require('../lib/auth');
 
-router.get('/signup', (req, res) => {
+router.get('/signup', isNotLoggedIn, (req, res) => {
   res.render('auth/signup');
 });
 
@@ -16,11 +17,11 @@ router.post(
   })
 );
 
-router.get('/signin', (req, res) => {
+router.get('/signin', isNotLoggedIn, (req, res) => {
   res.render('auth/signin');
 });
 
-router.post('/signin', (req, res, next) => {
+router.post('/signin', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local.signin', {
     successRedirect: '/profile',
     failureRedirect: '/signin',
@@ -28,8 +29,13 @@ router.post('/signin', (req, res, next) => {
   })(req, res, next);
 });
 
-router.get('/profile', (req, res) => {
-  res.send('This is your profile');
+router.get('/profile', isLoggedIn, (req, res) => {
+  res.render('profile');
+});
+
+router.get('/logout', isLoggedIn, (req, res) => {
+  req.logout();
+  res.redirect('/signin');
 });
 
 module.exports = router;
--- src/routes/links.js
+++ src/routes/links.js
@@ -1,13 +1,14 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-const pool = require("../database");
+const pool = require('../database');
+const { isLoggedIn } = require('../lib/auth');
 
-router.get("/add", (req, res) => {
-  res.render("links/add");
+router.get('/add', isLoggedIn, (req, res) => {
+  res.render('links/add');
 });
 
-router.post("/add", async (req, res) => {
+router.post('/add', isLoggedIn, async (req, res) => {
   const { title, url, description } = req.body;
   const newLink = {
     title,
@@ -15,30 +16,30 @@ router.post("/add", async (req, res) => {
     description,
   };
 
-  await pool.query("INSERT INTO links set ?", [newLink]);
-  req.flash("success", "Link saved successfully");
-  res.redirect("/links");
+  await pool.query('INSERT INTO links set ?', [newLink]);
+  req.flash('success', 'Link saved successfully');
+  res.redirect('/links');
 });
 
-router.get("/", async (req, res) => {
-  const links = await pool.query("SELECT * FROM links");
-  res.render("links/list", { links });
+router.get('/', isLoggedIn, async (req, res) => {
+  const links = await pool.query('SELECT * FROM links');
+  res.render('links/list', { links });
 });
 
-router.get("/delete/:id", async (req, res) => {
+router.get('/delete/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
-  await pool.query("DELETE FROM links WHERE ID = ?", [id]);
-  req.flash("success", "Link removed successfully");
-  res.redirect("/links");
+  await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+  req.flash('success', 'Link removed successfully');
+  res.redirect('/links');
 });
 
-router.get("/edit/:id", async (req, res) => {
+router.get('/edit/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
-  const links = await pool.query("SELECT * FROM links WHERE id = ?", [id]);
-  res.render("links/edit", { link: links[0] });
+  const links = await pool.query('SELECT * FROM links WHERE id = ?', [id]);
+  res.render('links/edit', { link: links[0] });
 });
 
-router.post("/edit/:id", async (req, res) => {
+router.post('/edit/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const { title, description, url } = req.body;
   const newLink = {
@@ -46,10 +47,9 @@ router.post("/edit/:id", async (req, res) => {
     description,
     url,
   };
-  console.log(newLink);
-  await pool.query("UPDATE links set ? WHERE id = ?", [newLink, id]);
-  req.flash("success", "Link updated successfully");
-  res.redirect("/links");
+  await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
+  req.flash('success', 'Link updated successfully');
+  res.redirect('/links');
 });
 
 module.exports = router;
