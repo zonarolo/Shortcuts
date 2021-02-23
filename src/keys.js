d4ac078a41bae9a199bd2b2364ea7f860b545fc1
zonarolo <juanluis211@outlook.com>
2021-02-23 23:22:52 +0100

    Screenshots

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,8 @@
 module.exports = {
-
   database: {
     host: 'localhost',
     user: 'root',
-    password: 'juangax37u',
-    database: 'database_links'
-  }
-
+    password: 'Juangax37u-',
+    database: 'database_links',
+  },
 };
